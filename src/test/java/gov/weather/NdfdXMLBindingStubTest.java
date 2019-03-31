855a42576cb7e2be920e086433ae854f922abbcc
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-03-31 16:24:51 -0500

    init commit

---
+++ src/test/java/gov/weather/NdfdXMLBindingStubTest.java
@@ -0,0 +1,39 @@
+package gov.weather;
+
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.rmi.server.ExportException;
+
+import static org.junit.Assert.*;
+
+public class NdfdXMLBindingStubTest {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Test
+    public void latLonListZipCode() throws Exception {
+        String latlongstring = null;
+
+        NdfdXMLBindingStub binding = (NdfdXMLBindingStub) new NdfdXMLLocator().getndfdXMLPort();
+        String res = binding.latLonListZipCode("53562");
+
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            DwmlType latlong = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(res));
+            latlongstring = latlong.getLatLonList();
+
+            logger.info(latlong);
+        } catch (JAXBException e) {
+            logger.error("Unmarshalling failed", e);
+        }
+
+        assertEquals("Failed", "43.1029,-89.5063", latlongstring);
+    }
+}
